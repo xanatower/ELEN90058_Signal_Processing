29d9e3ce7c5107d18b6f0a867185684c42d53fb9
Shiyu Zhang <shiyuz2@student.unimelb.edu.au>
2017-08-14 19:05:35 -0400

    workshop1_b_e

---
+++ SPWS1.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <math.h>
+
+// Globals
+#define N	256
+#define	PI	3.1415
+
+float x[N];
+float y1[N];
+float y2[N];
+
+int main(void)
+{
+	int i;
+	float omega1 = 0.25 * PI, omega2 = 1.9 * PI;
+	float T2 = 1/4.8;
+	float T = 1.0;
+	float a=0.12;
+	float alpha1 = 0.593, alpha2 = 0.464;
+
+	x[0]  = exp(-a*0*T2)*cos(omega1*0*T2) + 0.1*sin(omega2*0*T2);
+	y1[0] = ((1-alpha1)/2)*x(1);
+	y2[0] = y2(1) = ((1-alpha2)/2)*y1(1);
+	for (i = 0; i < N; i++)
+	{
+		x[i] = exp(-a*i*T2)*cos(omega1*i*T2) + 0.1*sin(omega2*i*T2);
+		y1[i] = ((1-alpha1)/2)*x(n)+((1-alpha1)/2)*x(n-1)+alpha1*y1(n-1);
+    	y2[i] = ((1-alpha2)/2)*y1(n)+((1-alpha2)/2)*y1(n-1)+alpha2*y2(n-1);
+	}
+
+/*	for (i = 0; i < N; i++)
+	{
+		printf("x[%d] = %f\n", i, x[i]);
+	}
+*/
+	printf("Done.\n");
+
+	return 0;
+}
+
