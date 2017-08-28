0d894886b2ca3fbdc71b0f762eee6b3a83a75dd9
Shiyu Zhang <steve@biofuelinnovations.com.au>
2017-08-29 09:39:37 +1000

    add_filter

--- Workshop 2/SPWS2-echo/src/_LabTasks.c
+++ Workshop 2/SPWS2-echo/src/_LabTasks.c
@@ -8,12 +8,17 @@ float RightInput;
 float loa, lob, loc;
 
 // Declare any global variables you need
+int D = 1760;
+float alpha_a  = 0.75;
+float x[D] = {0.0};
+int current = 0;
+
 
 void EchoFilter(void)
 {
 	// TODO: Implement echo filter (a)
 	//loa = LeftInput;
-	loa = LeftInput + alpha_a * 
+	loa = LeftInput + alpha_a * x[current];
 
 	// TODO: Implement echo filter (b)
 	lob = LeftInput;
@@ -22,5 +27,7 @@ void EchoFilter(void)
 	loc = LeftInput;
 
 	//update the sample 
-	
+	x[current] = LeftInput;
+	current++;
+	current = current%4;
 }
